7bd121d75e341a51d762cbcf9a98403caf0b1d55
daisysalem <62805119+daisysalem@users.noreply.github.com>
2020-04-20 15:48:33 -0700

    Basic JavaScript Projects Folder
    Per JAVASCRIPT COURSE - Step 63

---
+++ Basic JavaScript Projects/JS/Basic_ JavaScript_2.js
@@ -0,0 +1,21 @@
+var a = "This is red.<br>";
+var a = a.fontcolor("red");
+
+var b = "This is blue.<br>";
+var b = b.fontcolor("blue");
+
+document.write (a);
+document.write (b);
+
+function myFunction2()   {
+    var sentence = "You just clicked me!";
+    sentence += " Wasn't that cool!?";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
+
+document.write (sentence);
+
+
+
+
+
