07dd82a22fe1d82f94e84c2b79d0ddb8bd8f3981
daisysalem <62805119+daisysalem@users.noreply.github.com>
2020-04-20 14:12:09 -0700

    Basic JavaScript Projects Folder
    Per JAVASCRIPT COURSE Step 58

---
+++ Basic JavaScript Projects/JS/Basic_ JavaScript_1.js
@@ -0,0 +1,36 @@
+window.alert("Hello, World!");
+
+document.write("Hello, World!<br>");
+
+var A = "This is a string.<br>";
+var A = A.fontcolor("red");
+
+document.write(A);
+
+var b = "Here's another string.";
+window.alert(b);
+
+var d = "Here's another string.";
+var d = d.fontcolor("green");
+
+document.write("\"Be the change you wish to see in the world\" -Ghandi<br>");
+
+document.write("I don\'t understand" + " why JS can\'t read apostrophes without using a \\.  Can\'t the original Programmer fix this?" + "\ -Daisy<br>");
+
+var c = A + b;
+var c = c.fontcolor("blue");
+
+document.write(c+"<br>");
+
+
+var Family = "The Salems", Mom="Daisy", Son="Brandon", Daughter="Jasmine";
+var Mom = Mom.fontcolor("purple");
+
+document.write(Mom+"<br>");
+
+document.write(3+3);
+
+
+
+
+
